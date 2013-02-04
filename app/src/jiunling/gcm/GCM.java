369704f8e754b130c1b0a9429b769991660ef1ad
jiunjiun <quietmes@gmail.com>
2013-02-05 02:10:48 +0800

    Add Google Cloud Messaging for Android service. Add Wifi, Internet, WAKE_LOCK permission.

---
+++ app/src/jiunling/gcm/GCM.java
@@ -0,0 +1,41 @@
+package jiunling.gcm;
+
+import static jiunling.config.config.SENDER_ID;
+import static jiunling.config.config.UserRegistrarId;
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gcm.GCMRegistrar;
+
+public class GCM {
+	//Debugging
+	private static final String TAG = "GCM";
+	private static final boolean D = true;
+	
+	public static String regId = "";
+	
+	Context mContext = null;
+
+	public GCM( Context mContext ){
+		this.mContext = mContext;
+		this.Registrar();
+	}
+	
+	private void Registrar(){
+		GCMRegistrar.checkDevice(mContext);
+    	GCMRegistrar.checkManifest(mContext);
+    	regId = GCMRegistrar.getRegistrationId(mContext);
+    	if (regId.equals("")) {
+    		if(D) Log.e(TAG, "+++ regId.equals +++");
+    		GCMRegistrar.register(mContext, SENDER_ID);
+    	} else {
+    		Log.e(TAG, "regId: "+regId);
+    		UserRegistrarId = regId;
+    	}
+	}
+	
+	public void Unregister(){
+        GCMRegistrar.onDestroy(mContext);
+	}
+
+}
---
+++ app/src/jiunling/pass/GCMIntentService.java
@@ -0,0 +1,59 @@
+package jiunling.pass;
+
+import static jiunling.config.config.EXTRA_MESSAGE;
+import static jiunling.config.config.SENDER_ID;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gcm.GCMBaseIntentService;
+import com.google.android.gcm.GCMRegistrar;
+
+public class GCMIntentService extends GCMBaseIntentService {
+	
+	private static final boolean D = true;
+    private static final String TAG = "GCMIntentService";
+   
+    public GCMIntentService() {
+        super(SENDER_ID);
+    }
+
+    @Override
+    protected void onRegistered(Context context, String registrationId) {
+        Log.e(TAG, "Device registered: regId = " + registrationId);
+    }
+
+    @Override
+    protected void onUnregistered(Context context, String registrationId) {
+        Log.i(TAG, "Device unregistered");
+        if (GCMRegistrar.isRegisteredOnServer(context)) {
+
+        } else {
+            // This callback results from the call to unregister made on
+            // ServerUtilities when the registration to the server failed.
+            Log.i(TAG, "Ignoring unregister callback");
+        }
+    }
+
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        if(D) Log.e(TAG, " -- message -- " + message);
+    }
+
+    @Override
+    protected void onDeletedMessages(Context context, int total) {
+        Log.i(TAG, "Received deleted messages notification");
+    }
+
+    @Override
+    public void onError(Context context, String errorId) {
+        Log.i(TAG, "Received error: " + errorId);
+    }
+
+    @Override
+    protected boolean onRecoverableError(Context context, String errorId) {
+        Log.i(TAG, "Received recoverable error: " + errorId);
+        return super.onRecoverableError(context, errorId);
+    }
+}
