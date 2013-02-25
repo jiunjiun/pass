75eb2ad0cbab66aa58529c5bbf11b327cc204df9
jiunjiun <quietmes@gmail.com>
2013-02-26 06:18:27 +0800

    Create rails object

---
+++ web/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T::Application.config.secret_token = '976619ebaae56ff625b0575c15d8452e3ad344455d3a65f585039cc44d0086d1989baaa50d5cfe2134074f513c75204ee827e54453a3b01f74013a6595ea9ce2'
