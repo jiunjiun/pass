74ed61963aa989c698970d100675df8bec86f1ef
jiunjiun <quietmes@gmail.com>
2013-03-01 02:58:58 +0800

    Add config.sample and Edit api.py

--- web/GAE(python)/controllers/api.py
+++ web/GAE(python)/controllers/api.py
@@ -16,60 +16,50 @@
 #
 import webapp2
 import urllib
+
 from google.appengine.ext.webapp import template
-from config import config
+
 
 class index(webapp2.RequestHandler):
 	def get(self):
-		self.response.out.write(template.render('template/layout.html',''))
+		self.error(404)
 
 class register(webapp2.RequestHandler):		
 	def get(self):
-		from django.utils import simplejson as json
-		jsonstr = []
-		user_email = self.request.get('gcm')
-		gcm = self.request.get('gcm')
+		self.error(404)
 		
-		jsonstr = {'user_email' : user_email, 'gcm' : gcm}
-		jsonEncode = json.dumps( jsonstr )
-
+	def post(self):
+		import sys  
 		from google.appengine.api import urlfetch
-		url = "http://localhost/"
-		form_fields = {
-		  "first_name": "Albert",
-		  "last_name": "Johnson",
-		  "email_address": "Albert.Johnson@example.com"
-		}
-		form_data = urllib.urlencode(form_fields)
-		result = urlfetch.fetch(url=url,
-								payload=form_data,
-								method=urlfetch.POST,
-								headers={'Content-Type': 'application/x-www-form-urlencoded'})
+		from django.utils import simplejson
+		from config import config
 		
-		if result.status_code == 200:
-			self.response.write(result.content)
-		else:
-			self.response.write('error')
+		sys.path.append('..')
+		SERVER_URL = config.SERVER_URL
+		try:
+			reg	= simplejson.loads(self.request.get('reg'))			
+			form_data = 'test[email]=%s&test[gcm]=%s' % (reg['email'], reg['gcm'])
+			result = urlfetch.fetch(url=SERVER_URL, payload=form_data, method=urlfetch.POST)
+		except Exception: 
+			pass
+		
+class save(webapp2.RequestHandler):	
+	def get(self):
+		self.error(404)
 		
 	def post(self):
-		# from django.utils import simplejson as json
-		# user_email = self.request.get('gcm')
-		# gcm = self.request.get('gcm')
-		# result = json.dumps( mac )
-		self.response.write('')
-		
-class find(webapp2.RequestHandler):		
-	def post(self):
-		from django.utils import simplejson as json
-		mac = self.request.get('m')
-		result = json.dumps( mac )
-		self.response.write(result)
-		
-class save(webapp2.RequestHandler):		
-	def post(self):
-		from django.utils import simplejson as json
-		mac = self.request.get('m')
-		result = json.dumps( mac )
-		self.response.write(result)
+		import sys  
+		from google.appengine.api import urlfetch
+		from django.utils import simplejson
+		from config import config
+
+		sys.path.append('..')
+		SERVER_URL = config.SERVER_URL
+		try:
+			reg	= simplejson.loads(self.request.get('reg'))			
+			form_data = 'test[email]=%s&test[gcm]=%s' % (reg['email'], reg['gcm'])
+			result = urlfetch.fetch(url=SERVER_URL, payload=form_data, method=urlfetch.POST)
+		except Exception: 
+			pass
 
 		
