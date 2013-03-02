7f44386b8f1c3747788fc7cbb785fbe8c0dbf2f7
jiunjiun <quietmes@gmail.com>
2013-03-03 02:58:54 +0800

    Remove rails object

--- web/local_rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-LocalRails::Application.config.secret_token = 'd197f66661c4da795c95a20ff7bfe4abd8e7fb456e307b73aaf5e78c48d29383d3adaae3297248d56aa089364953b17ebfc6dc0a5d137820bf1882c114c1e452'
--- web/local_rails/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-LocalRails::Application.config.session_store :cookie_store, key: '_local_rails_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# LocalRails::Application.config.session_store :active_record_store
