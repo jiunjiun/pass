7ed0719bb3fb17dc000b748aaea7bbb9a524db89
jiunjiun <quietmes@gmail.com>
2013-03-04 11:12:56 +0800

    Add Register from GAE website

--- web/GAE(python)/controllers/api.py
+++ web/GAE(python)/controllers/api.py
@@ -24,24 +24,31 @@ class index(webapp2.RequestHandler):
 	def get(self):
 		self.error(404)
 
-class register(webapp2.RequestHandler):		
+class register(webapp2.RequestHandler):	
 	def get(self):
 		self.error(404)
 		
 	def post(self):
-		import sys  
+		import json, sys, re
 		from google.appengine.api import urlfetch
-		from django.utils import simplejson
 		from config import config
 		
 		sys.path.append('..')
 		SERVER_URL = config.SERVER_URL
+		
 		try:
-			reg	= simplejson.loads(self.request.get('reg'))			
-			form_data = 'user[email]=%s&user[gcm]=%s' % (reg['email'], reg['gcm'])
-			result = urlfetch.fetch(url=SERVER_URL, payload=form_data, method=urlfetch.POST)
-		except Exception: 
-			pass
+			reg 			= self.request.get('reg')
+			reg_arr			= json.loads(reg)
+			reg_arr['gps'] 	= json.loads(reg_arr['gps']) 
+
+			if len(reg_arr['email']) > 0 and re.match(r"[^@]+@[^@]+\.[^@]+", reg_arr['email']) and len(reg_arr['gcm']) > 0 and len(reg_arr['gps']) > 0 and len(reg_arr['gps']['lat']) > 0 and len(reg_arr['gps']['long']) > 0:
+				form_data 	= 'reg=%s&k=reg' % (reg)
+				result = urlfetch.fetch(url=SERVER_URL, payload=form_data, method=urlfetch.POST)
+				self.response.out.write(result.content)
+			else:
+				self.error(404)
+		except Exception, e: 
+			self.error(404)
 		
 class save(webapp2.RequestHandler):	
 	def get(self):
