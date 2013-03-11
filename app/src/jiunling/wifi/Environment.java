da7e8d011b5559d163cc41ae839ace808acd71d1
jiunjiun <quietmes@gmail.com>
2013-03-12 03:15:52 +0800

    Edit Environment 監聽 WIFI 並比對 database

--- app/src/jiunling/wifi/Environment.java
+++ app/src/jiunling/wifi/Environment.java
@@ -1,7 +1,10 @@
 package jiunling.wifi;
 
+import jiunling.SQLite.WiFiDb;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.util.Log;
 
 public class Environment {
@@ -30,11 +33,58 @@ public class Environment {
 	}
 		
 	public void ScanHaveSpecifiedWifi() {
-		if(D) Log.e(TAG, "scan wifi");
-		UpdateWifiHelper();
-		for (ScanResult result : mWifiHelper.getWifiList()) {
-			if(D) Log.e(TAG, result.SSID + " " + result.level + " " + result.BSSID);
+		try {
+			UpdateWifiHelper();
+			if(mWifiHelper.getWifiStatus()) {
+				for (ScanResult result : mWifiHelper.getWifiList()) {
+					if(D) Log.e(TAG, result.toString());
+					if(D) Log.e(TAG, result.SSID + " " + result.level + " " + result.BSSID);
+					if(result.level > -70) {
+						WiFiDb dbHelper = new WiFiDb(mContext);
+						Cursor cursor 	= dbHelper.SWhere(new String[]{result.BSSID});
+				    	int rows_num 	= cursor.getCount();
+				    	if(rows_num != 0){
+							cursor.moveToFirst();			//將指標移至第一筆資料
+							for(int i=0; i<rows_num; i++) {
+								if(D) Log.e(TAG, "psk " + cursor.getString(0));
+								WifiConnect(result, cursor.getString(0));
+						        cursor.moveToNext();
+							}
+						}
+				    	cursor.close();		//關閉Cursor
+						dbHelper.close();	//關閉資料庫，釋放記憶體
+					}
+				}		
+			}
+		} catch(Exception e) {
+			if(D) Log.e(TAG, "err: "+e);
 		}
 	}
 	
+	private void WifiConnect(ScanResult result , String PASSWD) {
+		WifiConfiguration wc = null;
+		if(D) Log.e(TAG, "result.capabilities: "+result.capabilities);
+		if(PASSWD.equals("")) {
+			wc = mWifiHelper.ConnectWifi(result.SSID, PASSWD, WifiHelper.WifiCipher_NOPASSWD);
+		} else if(result.capabilities.indexOf("WEP") != -1) {
+			wc = mWifiHelper.ConnectWifi(result.SSID, PASSWD, WifiHelper.WifiCipher_WEP);
+		} else if(result.capabilities.indexOf("WPA") != -1) {
+			wc = mWifiHelper.ConnectWifi(result.SSID, PASSWD, WifiHelper.WifiCipher_WPA);
+		} else {
+			if(D) Log.e(TAG, "Null");
+		}
+		
+		mWifiHelper.addNetWordLink(wc);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
