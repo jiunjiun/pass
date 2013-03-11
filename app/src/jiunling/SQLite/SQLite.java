e896b29f381508674db1d0274489bce3a5f41055
jiunjiun <quietmes@gmail.com>
2013-03-12 03:02:02 +0800

    設定 SQL 初始 SQL 和設定 WIFI database function

--- app/src/jiunling/SQLite/SQLite.java
+++ app/src/jiunling/SQLite/SQLite.java
@@ -1,26 +1,22 @@
 package jiunling.SQLite;
 
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class SQLite extends SQLiteOpenHelper {
 	
 	/***	Debugging	***/
-	private static final String TAG = "SQLite";
-	private static final boolean D = true;
+//	private static final String TAG = "SQLite";
+//	private static final boolean D = true;
 	
 	/***	SQLite initial	***/
-	private static final String DATABASE_NAME = "BaoBao.db";	//資料庫名稱
-	private static final int DATABASE_VERSION = 2;	//資料庫版本
+	private static final String DATABASE_NAME = "WifiPass.db";	//資料庫名稱
+	private static final int DATABASE_VERSION = 1;	//資料庫版本
 
 	/***	SQLite Database	***/
 	public SQLiteDatabase db;
-	private static final String RouterList = "routerList";
+	public final String RouterList = "routerList";
 	
 	public SQLite(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -30,12 +26,12 @@ public class SQLite extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
-//		if(D) Log.e(TAG, "-- onCreate --");
+//		if(D) Log.e(TAG, "SQLite -- onCreate --");
 		String DATABASE_RouterList_TABLE =
 			    "create table "+ RouterList +" (" +
 			        "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-			        "jo454 	VARCHAR(255), " +
-			        "au4a83 VARCHAR(255), " +
+			        "jo454 	TEXT, " +
+			        "au4a83 TEXT " +
 			        ");";
 		//建立config資料表，詳情請參考SQL語法
 		db.execSQL(DATABASE_RouterList_TABLE);
@@ -47,36 +43,4 @@ public class SQLite extends SQLiteOpenHelper {
 		db.execSQL("DROP TABLE IF EXISTS "+RouterList);		//刪除舊有的資料表
 		onCreate(db);
 	}
-	
-	
-	/*************************************************************************************/
-	/***	User	***/
-	public long Create(String jo454, String au4a83){
-		ContentValues args = new ContentValues();
-		args.put("jo454", jo454);
-		args.put("au4a83", au4a83);
-		
-		return db.insert(RouterList, null, args);
-	}
-	
-	public Cursor SWhere(String jo454[]) throws SQLException {
-		Cursor cursor = db.query(true,
-				RouterList,					//資料表名稱
-				new String[] {"au4a83"},	//欄位名稱
-				"jo454=?",					//WHERE
-				jo454, 						// WHERE 的參數
-				null, 						// GROUP BY
-				null, 						// HAVING
-				null, 						// ORDOR BY
-				null  						// 限制回傳的rows數量
-		);
-	 
-		// 注意：不寫會出錯
-		if (cursor != null) {
-			cursor.moveToFirst();	//將指標移到第一筆資料
-		}
-		return cursor;
-	}
-	/*************************************************************************************/
-	
 }
---
+++ app/src/jiunling/SQLite/WiFiDb.java
@@ -0,0 +1,50 @@
+package jiunling.SQLite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+
+public class WiFiDb extends SQLite {
+	
+	/***	Debugging	***/
+//	private static final String TAG = "WiFiDb";
+//	private static final boolean D = true;
+	
+	public WiFiDb(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+	/***	Wifi	***/
+	public long Create(String jo454, String au4a83){
+		ContentValues args = new ContentValues();
+		args.put("jo454", jo454);
+		args.put("au4a83", au4a83);
+
+		return db.insert(RouterList, null, args);
+	}
+	
+	public Cursor SWhere(String[] jo454) throws SQLException {
+		Cursor cursor = db.query(true,
+				RouterList,					//資料表名稱
+				new String[] {"au4a83"},	//欄位名稱
+				"jo454=?",					//WHERE
+				jo454, 						// WHERE 的參數
+				null, 						// GROUP BY
+				null, 						// HAVING
+				null, 						// ORDOR BY
+				null  						// 限制回傳的rows數量
+		);
+	 
+		// 注意：不寫會出錯
+		if (cursor != null) {
+			cursor.moveToFirst();	//將指標移到第一筆資料
+		}
+		return cursor;
+	}
+	
+	public long DeleteDB(){
+		return db.delete(RouterList, null, null);
+	}
+}
