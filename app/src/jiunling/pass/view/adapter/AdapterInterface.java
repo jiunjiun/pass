3c27546889f5ac2d0cd94d371f943c340b719f9c
jiunjiun <quietmes@gmail.com>
2013-03-12 19:06:54 +0800

    Edit AdapterInterface.java OptionListViewAdapter.java, listview_main.xml test

--- app/src/jiunling/pass/view/adapter/AdapterInterface.java
+++ app/src/jiunling/pass/view/adapter/AdapterInterface.java
@@ -1,5 +1,35 @@
 package jiunling.pass.view.adapter;
 
-public class AdapterInterface {
+import android.app.Activity;
+import android.widget.ListView;
 
+public class AdapterInterface {
+	/***	Debugging	***/
+//	private static final String TAG = "AdapterInterface";
+//	private static final boolean D = true;
+	
+	private Activity mActivity;
+	
+	/**		layout	**/
+	private ListView mListView;
+	
+//	public static final int Option_ListView  = 1;
+	
+	public AdapterInterface(Activity activity , ListView mListView){
+		this.mActivity = activity;
+		this.mListView = mListView;
+	}
+	
+	public void ListViewKey(int Kind) {
+//		switch(Kind) {
+//		case Option_ListView:
+//			OptionListView();
+//			break;
+//		}
+	}
+	
+	private void OptionListView() {
+//		OptionListViewAdapter mOptionListViewAdapter = 
+//				new OptionListViewAdapter(mActivity, mListView);
+	}
 }
--- app/src/jiunling/pass/view/adapter/OptionListViewAdapter.java
+++ app/src/jiunling/pass/view/adapter/OptionListViewAdapter.java
@@ -1,5 +1,63 @@
 package jiunling.pass.view.adapter;
 
-public class OptionListViewAdapter {
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+public class OptionListViewAdapter {
+	
+	/***	Debugging	***/
+//	private static final String TAG = "CalendarListViewAdapter";
+//	private static final boolean D = true;
+	
+	private Activity mActivity;
+	private ListView mListView;
+	
+	public OptionListViewAdapter(Activity mActivity, ListView mListView) {
+		this.mActivity = mActivity;
+		this.mListView = mListView;
+	}
+	
+	public static class ViewHolder {
+		
+	}
+	
+	class SimpleAdapter extends BaseAdapter implements OnItemClickListener {
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			// TODO Auto-generated method stub
+			
+		}  
+		
+	}
 }
