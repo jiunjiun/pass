cef165909cca990c800e78d2fc9ce262fc08ef3b
jiunjiun <quietmes@gmail.com>
2013-03-14 17:09:08 +0800

    Create ScanPublicWifi.java scan public wifi and save to database

---
+++ app/src/jiunling/pass/wifi/ScanPublicWifi.java
@@ -0,0 +1,67 @@
+package jiunling.pass.wifi;
+
+import static jiunling.pass.service.BackgroundService.mGPS;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jiunling.pass.SQLite.PublicWifiDb;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.util.Log;
+
+public class ScanPublicWifi {
+	
+	/***	Debugging	***/
+	private static final String TAG = "ScanPublicWifi";
+	private static final boolean D = true;
+	
+	private Context mContext;
+
+	private WifiHelper mWifiHelper = null;
+	
+	public ScanPublicWifi(Context mContext) {
+		this.mContext = mContext;
+		mWifiHelper = new WifiHelper(mContext);
+	}
+	
+	private void UpdateWifiHelper() {
+		mWifiHelper.startScan();
+	}
+	
+	public synchronized void Scan() {
+		try {
+			UpdateWifiHelper();
+			if(mWifiHelper.getWifiStatus()) {
+				for(ScanResult result : mWifiHelper.getWifiList()) {
+					if(result.capabilities.indexOf("[ESS]") == 0) {
+						Save(result.SSID, result.BSSID);
+					}
+				}
+			}
+		} catch(Exception e) {
+			
+		}
+	}
+	
+	private void Save(String SSID, String MAC) {
+		String GPS = getGPS();
+		PublicWifiDb mPublicWifiDb = new PublicWifiDb(mContext);
+		if(D) Log.e(TAG, "SSID: "+ SSID +" MAC "+ MAC +" GPS " + GPS);
+		long l = mPublicWifiDb.Create(SSID, MAC, GPS);
+		if(D) Log.e(TAG, "return: "+ l);
+	}
+	
+	private String getGPS() {
+		Map<String, String> gps_params = new HashMap<String, String>();
+		if(mGPS != null) {
+			gps_params.put("lat", mGPS.getLatitude());
+			gps_params.put("long", mGPS.getLongitude());
+		}
+		JSONObject mJSONGPS = new JSONObject(gps_params);
+		return mJSONGPS.toString();
+	}
+}
