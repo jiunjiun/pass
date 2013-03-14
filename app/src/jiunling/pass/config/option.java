6e3a61dd8f8eb6d782ac1033379d76cb7a2df097
jiunjiun <quietmes@gmail.com>
2013-03-14 11:55:06 +0800

    Add option Save option var

---
+++ app/src/jiunling/pass/config/option.java
@@ -0,0 +1,16 @@
+package jiunling.pass.config;
+
+public class option {
+	/***	wifi ¥\²v ¶V¤p¶V®t	***/
+	public static final int Wifi_Level 					= -70;
+	
+	/**		Wifi setting	**/
+	public static boolean WifiScan						= true;
+	public static boolean NotificationUser				= false;
+	
+	private static final int Second 					= 1000;
+	public static int UpdateTime 						= 30;
+	public static int SleepTime							= UpdateTime * Second;
+	
+	
+}
