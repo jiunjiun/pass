c6ab21932a8cd038de9c27b62e4c2b7acef603dd
jiunjiun <quietmes@gmail.com>
2013-03-26 05:23:14 +0800

    Edit Environment.java 增加當連線後監聽WIFI的健康度並適時的丟掉取消連線

--- app/src/jiunling/pass/wifi/Environment.java
+++ app/src/jiunling/pass/wifi/Environment.java
@@ -1,5 +1,6 @@
 package jiunling.pass.wifi;
 
+import static jiunling.pass.config.Option.RssiLimit;
 import jiunling.pass.SQLite.WiFiDb;
 import android.content.Context;
 import android.content.Intent;
@@ -12,44 +13,34 @@ public class Environment {
 	
 	/***	Debugging	***/
 	private static final String TAG = "Environment";
-	private static final boolean D = false;
+	private static final boolean D = true;
 	
 	private Context mContext;
 		
 	/*** 	wifi	***/
-	private WifiHelper mWifiHelper = null;
-	
-	/***	MESSAGE format	***/
-	public static final int MESSAGE_REQUEST 		= 1;
-	public static final int MESSAGE_ERROR_EQUEST 	= 2;
-	
+	private WifiHelper mWifiHelper = null;	
 	
 	public Environment( Context mContext ) {
 		this.mContext = mContext;
 		mWifiHelper = new WifiHelper(mContext);
 	}
-	
-	private void UpdateWifiHelper() {
-		mWifiHelper.startScan();
-	}
 		
 	public synchronized void ScanHaveSpecifiedWifi() {
 		try {
-			UpdateWifiHelper();
+			mWifiHelper.startScan();
 			if(mWifiHelper.getWifiStatus()) {
-				if(D) Log.e(TAG, "mWifiHelper.getWifiList()== null: " + (mWifiHelper.getWifiList() == null));
 				if(mWifiHelper.getWifiList() != null) {
 					for (ScanResult result : mWifiHelper.getWifiList()) {
 						if(D) Log.e(TAG, "result.SSID: " + result.SSID);
 						if(result != null) {
-							if(result.level > -70) {
+							if(result.level >= RssiLimit) {
 								WiFiDb dbHelper = new WiFiDb(mContext);
 								Cursor cursor 	= dbHelper.SWhere(new String[]{result.BSSID});
 						    	int rows_num 	= cursor.getCount();
 						    	if(rows_num != 0) {
 									cursor.moveToFirst();			//將指標移至第一筆資料
 									for(int i=0; i<rows_num; i++) {
-										if(D) Log.e(TAG, "psk " + cursor.getString(0));
+//										if(D) Log.e(TAG, "psk " + cursor.getString(0));
 									    mContext.sendBroadcast(new Intent("isDataWifi"));
 										WifiConnect(result, cursor.getString(0));
 								        cursor.moveToNext();
@@ -69,7 +60,7 @@ public class Environment {
 	
 	private void WifiConnect(ScanResult result , String PASSWD) {
 		WifiConfiguration wc = null;
-		if(D) Log.e(TAG, "result.capabilities: "+result.capabilities);
+//		if(D) Log.e(TAG, "result.capabilities: "+result.capabilities);
 		if(PASSWD.equals("")) {
 			wc = mWifiHelper.ConnectWifi(result.SSID, PASSWD, WifiHelper.WifiCipher_NOPASSWD);
 		} else if(result.capabilities.indexOf("WEP") != -1) {
@@ -81,6 +72,22 @@ public class Environment {
 		}
 		
 		mWifiHelper.addNetWordLink(wc);
+	}
+	
+	public void MonitorWifi() {
+		try {
+			mWifiHelper = new WifiHelper(mContext);
+			if(mWifiHelper.getWifiStatus()) {
+				int Rssi = mWifiHelper.getRssi();
+				if(D) Log.e(TAG, "Rssi: "+ Rssi);
+				if(Rssi < RssiLimit) {
+					int NetId = mWifiHelper.getCurrentNetId();
+					mWifiHelper.removeNetworkLink(NetId);
+				}
+			}
+		} catch(Exception e) {
+			if(D) Log.e(TAG, "err: "+e);
+		}
 	}
 }
 
