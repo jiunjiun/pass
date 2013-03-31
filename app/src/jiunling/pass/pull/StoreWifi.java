37a8a250b37110356ac6a419c25f050e2fba597b
jiunjiun <quietmes@gmail.com>
2013-04-01 02:41:47 +0800

    Edit pull/StoreWifi.java Eidt pull wifis data, 因為資料量的關係，增加 index(目前資料量), length(總資料量)

--- app/src/jiunling/pass/pull/StoreWifi.java
+++ app/src/jiunling/pass/pull/StoreWifi.java
@@ -22,16 +22,28 @@ public class StoreWifi {
 	
 	public void Save(String Data) {
 		WiFiDb dbHelper = new WiFiDb(mContext);
-		
-		dbHelper.DeleteDB();
+
 		try {
-			JSONArray mJSONArray = new JSONArray(Data);
+			JSONObject mJSONObject = new JSONObject(Data);
+			if(Integer.parseInt(mJSONObject.getString("index")) == 0) {
+				dbHelper.DeleteDB();
+			}
+			
+			if(Integer.parseInt(mJSONObject.getString("length"))-1 == Integer.parseInt(mJSONObject.getString("index"))) {
+				if(D) Log.e(TAG, "finish save");
+			}
+			
+			JSONArray mJSONArray = new JSONArray(mJSONObject.getString("wifis"));
 			for(int i=0;i<mJSONArray.length();i++) {
-				JSONObject mJSONObject = mJSONArray.getJSONObject(i);
-	            String jo454 = mJSONObject.getString("jo454");
-	            String au4a83 = mJSONObject.getString("au4a83") != "null" ? mJSONObject.getString("au4a83") : "";
+				JSONObject wifis = mJSONArray.getJSONObject(i);
+	            String jo454 = wifis.getString("jo454");
+	            String au4a83 = wifis.getString("au4a83") != "null" ? wifis.getString("au4a83") : "";
 				if(D) Log.e(TAG, "jo454: "+ jo454 + " au4a83: "+ au4a83);
 				dbHelper.Create(jo454, au4a83);
+			}
+			
+			if(Integer.parseInt(mJSONObject.getString("length"))-1 == Integer.parseInt(mJSONObject.getString("index"))) {
+				if(D) Log.e(TAG, "finish save");
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
